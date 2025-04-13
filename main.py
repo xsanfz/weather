2d84db816dd51a3237347de14c19a06420e7902f
xsanfz <sergey.myamin@gmail.com>
2025-04-13 11:26:43 +0400

    Update main.py
    Отложил перехват исключения 
    Высушил код 
    Уточнил название функции

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import requests
 
 
-def fetch_weather_data(city):
-    try:
-        response = requests.get(
-            f"https://wttr.in/{city}",
-            params={'M': '', 'T': '', 'n': '', 'Q': '', 'lang': 'ru'},
-            timeout=5
-        )
-        response.raise_for_status()
-        return f"{city}: {response.text.strip()}"
-
-    except requests.exceptions.HTTPError:
-        return f"Ошибка: не удалось получить данные для {city} (HTTP ошибка)"
-    except requests.exceptions.ConnectionError:
-        return f"Ошибка соединения при запросе погоды для {city}"
-    except requests.exceptions.Timeout:
-        return f"Превышено время ожидания ответа для {city}"
-    except requests.exceptions.RequestException as e:
-        return f"Ошибка при запросе погоды для {city}: {str(e)}"
+def fetch_weather(city):
+    response = requests.get(
+        f"https://wttr.in/{city}",
+        params={'M': '', 'T': '', 'n': '', 'Q': '', 'lang': 'ru'},
+        timeout=5
+    )
+    response.raise_for_status()
+    return response.text
 
 
 def main():
     cities = ["Лондон", "Шереметьево", "Череповец"]
+
     for city in cities:
-        print(fetch_weather_data(city))
+        try:
+            forecast = fetch_weather(city).strip()
+            print(f"{city}: {forecast}")
+        except requests.exceptions.HTTPError:
+            print(f"Ошибка: не удалось получить прогноз для {city} (HTTP ошибка)")
+        except requests.exceptions.ConnectionError:
+            print(f"Ошибка соединения при запросе погоды в {city}")
+        except requests.exceptions.Timeout:
+            print(f"Превышено время ожидания ответа для {city}")
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при запросе погоды в {city}: {str(e)}")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Критическая ошибка: {str(e)}")
